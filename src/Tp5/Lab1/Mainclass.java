75b58f2b9fd18876307fde3e387b6ce43c2686c2
GonzaloBustos8 <163307288+GonzaloBustos8@users.noreply.github.com>
2024-03-29 16:29:57 -0300

    Initial commit

---
+++ src/Tp5/Lab1/Mainclass.java
@@ -0,0 +1,37 @@
+package Tp5.Lab1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author busto
+ */
+public class Mainclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+/** Crea una aplicación GUI en Java que gestione una lista de productos. La interfaz
+debe contener los siguientes elementos:
+
+Un ComboBox (JComboBox) que permita al usuario seleccionar una categoría de
+producto de una lista predefinida (por ejemplo, electrónica, ropa, alimentos, etc.).
+Un botón "Agregar producto" (JButton) que permita al usuario agregar un nuevo producto a la lista.
+Una tabla (JTable) que muestre los productos agregados, con columnas para el nombre del producto, la categoría y el precio.
+Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el
+botón "Agregar producto", el programa debe permitir al usuario ingresar el nombre y el precio
+del producto en campos de texto separados. Luego, debe agregar el producto a la tabla con la categoría seleccionada.
+
+Además, la tabla debe ser actualizada automáticamente cada vez que se agrega un nuevo producto.
+
+Subir proyecto a GitHub y enviar link del repositorio. */
+    }
+    
+}
